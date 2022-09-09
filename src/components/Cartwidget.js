a270793aa2d8a6be0c3dff20870ee66ab90f7330
Juan Romero <romerojuanmanuel1605@gmail.com>
2022-09-09 20:43:28 -0300

    Se gragó el carrito y el titulo

---
+++ src/components/Cartwidget.js
@@ -0,0 +1,11 @@
+const Cartwidget = () =>{
+    return (
+        <>
+            <div className="imgCart">
+              <img src="https://img.icons8.com/pastel-glyph/64/1A1A1A/shopping-cart--v1.png"/>
+            </div>
+        </>
+    );
+}
+
+export default Cartwidget;
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,15 @@
+const ItemListContainer = (props) =>{
+    return(
+    <>
+
+    <div>
+
+      <h1 className="titulo1">{props.greeting}</h1>
+
+    </div>
+
+    </>
+    );
+}
+
+export default ItemListContainer;
