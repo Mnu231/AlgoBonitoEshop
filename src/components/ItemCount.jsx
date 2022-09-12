2a0530183f4c0f2a50ce58f01e44377591ab13cd
Juan Romero <romerojuanmanuel1605@gmail.com>
2022-09-12 20:27:05 -0300

    Se agregó el contador

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,35 @@
+import  {useState} from "react"
+const ItemCount = ({stock , initial , onAdd}) =>{
+    
+
+    const [cantidad, setCantidad] = useState(1);
+
+    const addInitial = () =>{
+        if(cantidad < stock){
+            setCantidad (cantidad + 1)
+        }
+    }
+
+    const substractInitial = () =>{
+        if(cantidad > initial){
+            setCantidad (cantidad - 1)
+        }
+    }
+
+    
+    return(
+        <>
+        <div className="counter-container">
+            <div className="number-container">
+                <button onClick={substractInitial} type="button" class="btn btn-secondary">-</button>{cantidad}
+                <button onClick={addInitial} type="button" class="btn btn-secondary">+</button>
+            </div>
+            <div className="send-container">
+                <button className="btn btn-primary">Agregar al carrito</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,3 +1,5 @@
+import ItemCount from "./ItemCount";
+
 const ItemListContainer = (props) =>{
     return(
     <>
@@ -5,6 +7,7 @@ const ItemListContainer = (props) =>{
     <div>
 
       <h1 className="titulo1">{props.greeting}</h1>
+      <ItemCount stock = "5" initial = "1"/>
 
     </div>
 
